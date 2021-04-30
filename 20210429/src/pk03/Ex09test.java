a3862ce078d9a20779ed45760664f58b19eb16a1
sehji <sehji@DESKTOP-661U6P0>
2021-04-30 16:30:34 +0900

    2104301630박세진수정

--- 20210429/src/pk03/Ex09test.java
+++ 20210429/src/pk03/Ex09test.java
@@ -8,13 +8,13 @@ public class Ex09test {
 
 		HashMap<String, Ex09> map=new HashMap<String, Ex09>();
 		
-		Ex09 ex=new Ex09("asd", "asdfa@naver.com");
+		Ex09 ex=new Ex09("asd", "asdfa@naver.com","sdfds");
 		map.put(ex.userId, ex);
-		ex=new Ex09("asd1", "asdfa4@gmail.com");
+		ex=new Ex09("asd1", "asdfa4@gmail.com","sdf");
 		map.put(ex.userId, ex);
-		ex=new Ex09("asd2", "asdfa5@hanmail.net");
+		ex=new Ex09("asd2", "asdfa5@hanmail.net","sdf");
 		map.put(ex.userId, ex);
-		ex=new Ex09("asd3", "asdfa6@daum.net");
+		ex=new Ex09("asd3", "asdfa6@daum.net","asdf");
 		map.put(ex.userId, ex);
 		
 		
